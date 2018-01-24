d9ef0f91b0e87868fe01be37e8039e9543824067
akiyama_k <hechima.itouri@gmail.com>
2018-01-24 23:41:22 +0900

    Modity some problems

--- install_sgx.sh
+++ install_sgx.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
-if [ "$1" == "" ]; then
-    echo "please enter linux-sgx path"
-    exit 1
-fi
-
-LINUX_SGX_PATH="$1"
-
-CULLENT_PATH=$(pwd)
-
 set -eu
+
+# Build and Install the Intel(R) SGX Driver
+# Build the Intel(R) SGX Driver
+if [ ! -d linux-sgx-driver ]; then
+    git clone https://github.com/01org/linux-sgx-driver.git
+fi
+cd linux-sgx-driver/
+make -j8
+
+# Install the Intel(R) SGX Driver
+sudo mkdir -p "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx"    
+sudo cp isgx.ko "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx"    
+sudo sh -c "cat /etc/modules | grep -Fxq isgx || echo isgx >> /etc/modules"    
+sudo /sbin/depmod
+sudo /sbin/modprobe isgx
+cd ../ # ./
+
+# clone linux-sgx
+if [ ! -d linux-sgx ]; then
+    git clone https://github.com/01org/linux-sgx.git
+fi
 
 # Install Prerequisites
 sudo yum -y groupinstall 'Development Tools'
 sudo yum -y install ocaml wget python
 sudo yum -y install openssl-devel libcurl-devel protobuf-devel
 
-cd ${LINUX_SGX_PATH}
+cd linux-sgx # ./linux-sgx/
 ./download_prebuilt.sh
 
 # Build the Intel(R) SGX SDK and Intel(R) SGX PSW
@@ -28,18 +40,20 @@ make psw_install_pkg
 
 ./linux/installer/bin/sgx_linux_x64_sdk_2.1.42002.bin # TODO get version
 shopt -s expand_aliases # TODO understand this
-source ./sgxsdk/environment # TODO dont hard coding
+source ./sgxsdk/environment
 
 # Install the Intel(R) SGX PSW: Prerequisites
 wget "https://drive.google.com/uc?export=download&id=1LuEaM1iFpQJ-Y8jn54y2gdcT4vuBtW9O" -O iclsClient-1.45.449.12-1.x86_64.rpm
-# sudo rpm -ivh iclsClient-1.45.449.12-1.x86_64.rpm # TODO 
+sudo rpm -ivh iclsClient-1.45.449.12-1.x86_64.rpm # TODO check existence
 
 sudo yum install -y libuuid-devel libxml2-devel cmake pkgconfig
 
-# git clone https://github.com/intel/dynamic-application-loader-host-interface.git # TODO check exsitence  
-cd dynamic-application-loader-host-interface
+if [ ! -e dynamic-application-loader-host-interface]; then
+    git clone https://github.com/intel/dynamic-application-loader-host-interface.git
+fi
+cd dynamic-application-loader-host-interface # ./linux-sgx/dynamic-application-loader-host-interface/
 cmake .;make;sudo make install;sudo ldconfig;sudo systemctl enable jhi
-cd ../
+cd ../ # ./linux-sgx/
 
 # Install the Intel(R) SGX PSW
 ./linux/installer/bin/sgx_linux_x64_psw_2.1.42002.bin # TODO get version
