cdabb2a8f9780867ba2b42f41e9c6525d01197d4
itouri <k-akiyama@axsh.net>
2018-01-31 15:37:55 +0900

    sepalete sgx driver

--- install_sgx.sh
+++ install_sgx.sh
@@ -2,22 +2,6 @@
 
 set -eu
 
-# Build and Install the Intel(R) SGX Driver
-# Build the Intel(R) SGX Driver
-if [ ! -d linux-sgx-driver ]; then
-    git clone https://github.com/01org/linux-sgx-driver.git
-fi
-cd linux-sgx-driver/
-make -j8
-
-# Install the Intel(R) SGX Driver
-sudo mkdir -p "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx"    
-sudo cp isgx.ko "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx"    
-sudo sh -c "cat /etc/modules | grep -Fxq isgx || echo isgx >> /etc/modules"    
-sudo /sbin/depmod
-sudo /sbin/modprobe isgx
-cd ../ # ./
-
 # clone linux-sgx
 if [ ! -d linux-sgx ]; then
     git clone https://github.com/01org/linux-sgx.git
